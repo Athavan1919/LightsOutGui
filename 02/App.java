5a845bab0e24494f401aefc15fb91f240d4a762d
Aman Riat <amanriat777@gmail.com>
2019-03-23 12:30:10 -0400

    Cleaning up

--- 02/App.java
+++ 02/App.java
@@ -1,7 +1,7 @@
 public class App{
 	public static void main(String[] args) {
 
-        GameController j = new GameController(3,4);
+        GameController j = new GameController(3,3);
 
      
 
--- 02/GameController.java
+++ 02/GameController.java
@@ -2,6 +2,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
+import javax.swing.JOptionPane;
 
 // YOUR OTHER IMPORTS HERE IF NEEDED
 
